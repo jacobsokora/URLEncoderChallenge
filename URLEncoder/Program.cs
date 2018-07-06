68f13ca45c746d76be7478cbc1e0c781408472c6
Jacob <jacobsokora@gmail.com>
2018-07-06 13:56:52 -0500

    Fix control character error

--- URLEncoder/Program.cs
+++ URLEncoder/Program.cs
@@ -32,6 +32,12 @@ namespace URLEncoder
             {"`", "%60"}
         };
 
+        static List<int> controlCharacters = new List<int>(new int[] 
+        { 
+            0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
+            0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x7F
+        });
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the URL Encoder!");
@@ -56,7 +62,7 @@ namespace URLEncoder
                 valid = true;
                 foreach (char c in input.ToCharArray())
                 {
-                    if (Char.IsControl(c) || c == 0x7F)
+                    if (controlCharacters.Contains(c))
                     {
                         Console.WriteLine("Input contained an invalid character, please re-enter: ");
                         valid = false;
@@ -73,7 +79,7 @@ namespace URLEncoder
             {
                 value = value.Replace(conversion.Key, conversion.Value);
             }
-            return value
+            return value;
         }
     }
 }
